b1f0dc0bb3d0861fde6d96ffa2db68423dae206a
Justin Tormey <justin@blockchain.com>
2016-10-02 21:40:14 -0400

    basic functionality of app

---
+++ src/Helpers.elm
@@ -0,0 +1,5 @@
+
+module Helpers exposing (..)
+
+fromSatoshi : Float -> Float
+fromSatoshi n = n / 100000000
--- src/Main.elm
+++ src/Main.elm
@@ -1,36 +1,75 @@
 
 import Html exposing (..)
 import Html.App as App
-import Html.Attributes exposing (disabled)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+import Http
+import Json.Decode as Json exposing ((:=))
+import Task exposing (..)
+import String exposing (toFloat)
+import Helpers exposing (..)
 
 main =
-  App.beginnerProgram
-    { update = update
-    , model = model
+  App.program
+    { init = init
     , view = view
+    , update = update
+    , subscriptions = subscriptions
     }
 
-type alias Model = Int
+subscriptions : Model -> Sub Msg
+subscriptions model = Sub.none
 
-model : Model
+type alias Model =
+  { address: String
+  , balance: Float
+  }
+
+model: Model
 model =
-  0
+  { address = "1LeKvPwg5jpN9ragZ51b4jM6K8nWERCZXt"
+  , balance = 0
+  }
 
-type Msg = Increment | Decrement
+init : (Model, Cmd Msg)
+init =
+  (model, getBalance model.address)
 
-update : Msg -> Model -> Model
+type Msg = Address String | Balance Float | Failed Http.Error
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Increment ->
-      model + 1
-    Decrement ->
-      model - 1
+    Address address ->
+      ({ model | address = address }, Cmd.none)
+    Balance balance ->
+      ({ model | balance = balance }, Cmd.none)
+    Failed err ->
+      (model, Cmd.none)
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ span [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "inc" ]
-    , button [ onClick Decrement, disabled (model == 0)] [ text "dec" ]
-    ]
+  let
+    showBal = toString << fromSatoshi
+    bal =
+      div [ class "bal-container" ]
+        [ span [] [ text (if model.balance == 0 then "Error" else showBal model.balance) ]
+        ]
+    qr =
+      div [ class "qr-container" ]
+        [ img [ src ("https://blockchain.info/qr?data=" ++ model.address), width 150 ] []
+        ]
+    addr =
+      div [ class "addr-container" ]
+        [ span [] [ text model.address ]
+        ]
+  in
+    div [ class "container" ] [ bal, qr, addr ]
+
+getBalance : String -> Cmd Msg
+getBalance address =
+  let
+    url = "https://blockchain.info/balance?cors=true&active=" ++ address
+    decodeUrl = Json.at [ address, "final_balance" ] Json.float
+  in
+    Task.perform Failed Balance (Http.get decodeUrl url)
