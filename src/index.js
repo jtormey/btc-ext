4ef1721837f6be9489cd87d0c86a8d340b51dbe2
Justin Tormey <justin@blockchain.com>
2017-09-10 10:53:14 -0400

    refactor to modern js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
+import './main.scss'
+import { HDNode } from 'bitcoinjs-lib'
+import * as labels from './labels'
 
 // xpub6DX2ZjB6qgNGPuGobYQbpwXHrn7zue1xWSpg29cw6HxovCE9F4iHqEzjnhXk1PbKrfVGwMMrgQv7Q1wWDDBYzx85C8dsvD6jqc49U2PYstx
-
-require('./main.scss')
-let { HDNode } = require('bitcoinjs-lib')
-let labels = require('./labels')
 
 let deriveFromXpub = (xpub, index) => (
   HDNode.fromBase58(xpub).derive(0).derive(index).getAddress()
