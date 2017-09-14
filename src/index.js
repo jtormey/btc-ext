9be939121628c15c6ba16325dc56ba718a3fc814
Justin Tormey <justin@blockchain.com>
2017-09-14 19:00:53 -0400

    remove test xpub

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { setupPorts as setupBitcoinPorts } from './js/bitcoin'
 import { setupPorts as setupLabelsPorts } from './js/labels'
 import { setupPorts as setupStoragePorts } from './js/storage'
 
-// xpub6DX2ZjB6qgNGPuGobYQbpwXHrn7zue1xWSpg29cw6HxovCE9F4iHqEzjnhXk1PbKrfVGwMMrgQv7Q1wWDDBYzx85C8dsvD6jqc49U2PYstx
-
 let Elm = require('./elm/Main')
 let app = Elm.Main.embed(document.getElementById('main'))
 
