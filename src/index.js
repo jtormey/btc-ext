003060ce27670342c67ab01195d4d5eff8b22b63
Justin Tormey <justin@blockchain.com>
2017-09-14 18:38:22 -0400

    move labels ports js to labels module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import '../manifest.json?loadfile'
 import './style/main.scss'
-import * as labels from './js/labels'
 import { setupPorts as setupBitcoinPorts } from './js/bitcoin'
+import { setupPorts as setupLabelsPorts } from './js/labels'
 
 // xpub6DX2ZjB6qgNGPuGobYQbpwXHrn7zue1xWSpg29cw6HxovCE9F4iHqEzjnhXk1PbKrfVGwMMrgQv7Q1wWDDBYzx85C8dsvD6jqc49U2PYstx
 
@@ -9,6 +9,7 @@ let Elm = require('./elm/Main')
 let app = Elm.Main.embed(document.getElementById('main'))
 
 setupBitcoinPorts(app.ports)
+setupLabelsPorts(app.ports)
 
 app.ports.set.subscribe((data) => {
   let [key, value] = data.split(',')
@@ -23,15 +24,3 @@ app.ports.get.subscribe((key) => {
 app.ports.remove.subscribe((key) => {
   localStorage.removeItem(key)
 })
-
-app.ports.save.subscribe((data) => {
-  let [index, label] = data.split(',')
-  labels.save({ index: parseInt(index), label })
-})
-
-app.ports.readLabels.subscribe(() => {
-  let labelsStr = JSON.stringify(labels.read())
-  app.ports.readResponse.send(labelsStr)
-})
-
-app.ports.lastIndex.send(labels.lastIndex())
--- src/js/labels.js
+++ src/js/labels.js
@@ -1,16 +1,34 @@
 const STORE_KEY = 'labels'
 
-export const save = (entry) => {
-  let labels = read()
-  labels.push(entry)
-  localStorage.setItem(STORE_KEY, JSON.stringify(labels))
+class Labels {
+  save (entry) {
+    let labels = this.read()
+    labels.push(entry)
+    localStorage.setItem(STORE_KEY, JSON.stringify(labels))
+  }
+
+  read () {
+    return JSON.parse(localStorage.getItem(STORE_KEY) || '[]')
+  }
+
+  lastIndex () {
+    let labels = this.read()
+    return labels.map(l => l.index).reduce((a, i) => Math.max(a, i), 0)
+  }
 }
 
-export const read = () => {
-  return JSON.parse(localStorage.getItem(STORE_KEY) || '[]')
-}
+export const setupPorts = (ports) => {
+  let labels = new Labels()
 
-export const lastIndex = () => {
-  let labels = read()
-  return labels.map(l => l.index).reduce((a, i) => Math.max(a, i), 0)
+  ports.save.subscribe((data) => {
+    let [index, label] = data.split(',')
+    labels.save({ index: parseInt(index), label })
+  })
+
+  ports.readLabels.subscribe(() => {
+    let labelsStr = JSON.stringify(labels.read())
+    ports.readResponse.send(labelsStr)
+  })
+
+  ports.lastIndex.send(labels.lastIndex())
 }
