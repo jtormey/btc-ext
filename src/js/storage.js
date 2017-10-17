2415c7e51f0d6b9239098ee99a87fea0d5c831f2
Justin Tormey <jrtormey@gmail.com>
2017-10-16 22:09:26 -0400

    use chrome storage for syncing across browser

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,17 +1,40 @@
 const STORE_KEY = '@btc-ext'
 
+const fallbackStorage = {
+  set (kvs, callback) {
+    Object.keys(kvs).forEach(key => {
+      localStorage.setItem(key, kvs[key])
+    })
+    callback()
+  },
+  get (key, callback) {
+    let data = { [key]: localStorage.getItem(key) }
+    callback(data)
+  },
+  remove (key) {
+    localStorage.removeItem(key)
+  }
+}
+
+const storage = chrome.storage
+  ? chrome.storage.sync
+  : fallbackStorage
+
 export const setupPorts = (ports) => {
   ports.updateStorage.subscribe((data) => {
-    localStorage.setItem(STORE_KEY, data)
-    ports.receiveStorage.send(data)
+    storage.set({ [STORE_KEY]: data }, () => {
+      ports.receiveStorage.send(data)
+    })
   })
 
   ports.fetchStorage.subscribe(() => {
-    let value = localStorage.getItem(STORE_KEY)
-    if (value != null) ports.receiveStorage.send(value)
+    storage.get(STORE_KEY, (data) => {
+      let value = data[STORE_KEY]
+      if (value != null) ports.receiveStorage.send(value)
+    })
   })
 
   ports.clearStorage.subscribe(() => {
-    localStorage.removeItem(STORE_KEY)
+    storage.remove(STORE_KEY)
   })
 }
